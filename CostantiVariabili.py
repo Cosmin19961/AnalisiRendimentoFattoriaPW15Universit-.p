74beaacc2be0b897bc5d9166461e3680058b5cea
Cosmin19961 <vilca.cosmin@gmail.com>
2026-02-02 11:56:38 +0100

    Add cow lifespan and milk production calculations

--- CostantiVariabili.py
+++ CostantiVariabili.py
@@ -17,3 +17,28 @@ produzioneMediaAnnuaLattePerRazza = {
 
 for razza,produzione in produzioneMediaAnnuaLattePerRazza.items():
     print(razza,f"{produzione:.2f}","Litri/Giorno")
+#Genero una lista con l'eta minima e massima di vita della mucca
+
+#Da rivedere
+#Generatore di vita, quando l'oggetto MUCCA nasce, deve avere assegnato questa variabile, in modo che so quanto vivrà 
+generatoreCasualeEta = random.randint(1,15) #Generatore in ANNI
+generatoreCasualeEtaGiorni = generatoreCasualeEta * 365 #Generatore in giorni
+
+#genera una lista dei giorni casuali basata su quella precedente
+scorrereGiorniDiVita = list(range(1,generatoreCasualeEtaGiorni +1))
+
+#prendo in esame il massimo giorno di vita generato
+GiornoMassimoDiVita = scorrereGiorniDiVita[-1]
+
+#moltiplico i giorni di vita * produzione latte media
+ProduzioneMassimaLatteSuVitaMassimaStandard = GiornoMassimoDiVita * latteMedioGiorno
+
+#produzione latte media annuale
+ProduzioneMediaLatteSuVitaMassimaStandardAnnuale = ProduzioneMassimaLatteSuVitaMassimaStandard / generatoreCasualeEta 
+#Produzione latte media mensile
+ProduzioneMediaLatteSuVitaMassimaStandardMensile = ProduzioneMassimaLatteSuVitaMassimaStandard / generatoreCasualeEta / 12 
+
+print(f"La mucca ha vissuto per {generatoreCasualeEta} anni, equivalenti a {generatoreCasualeEtaGiorni} giorni")
+print(f"Produzione latte vita intera {ProduzioneMassimaLatteSuVitaMassimaStandard}")
+print(f"Produzione latte media annuale:{ProduzioneMediaLatteSuVitaMassimaStandardAnnuale}")
+print(f"Produzione latte media mensile:{ProduzioneMediaLatteSuVitaMassimaStandardMensile}")
