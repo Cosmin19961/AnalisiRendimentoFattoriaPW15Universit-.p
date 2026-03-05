44d6fe486b3709da587b4d9fb5a624941320d198
Cosmin19961 <vilca.cosmin@gmail.com>
2026-03-05 09:04:02 +0000

    risolto bug report

--- report.py
+++ report.py
@@ -8,9 +8,9 @@ Contenitore_Stampa_Report = {  # CAMBIO
     "capacita_massima_pastorizzazione": "Capacità massima di pastorizzazione:{pastorizzazione_reale_giorno_totale} litri",  # CAMBIO
     "scarto_mungitura": "Scarto Mungitura {scarto_mungitura_percent} %",  # CAMBIO
     "scarto_macchinari": "Scarto Macchinari {scarto_macchinari_percent} %",  # CAMBIO
-    "latte_munto_atteso": "Latte munto Atteso : {latte_giornaliero_atteso:.2f} litri",  # CAMBIO
-    "latte_pastorizzato_effettivo": "Latte pastorizzato effettivo : {latte_pastorizzato:.2f} litri",  # CAMBIO
-    "latte_non_pastorizzato": "Latte non pastorizzato: {latte_avanzato:.2f} litri",  # CAMBIO
+    "stampa_report("latte_munto_atteso", latte_giornaliero_atteso=latte_giornaliero_atteso),
+    "stampa_report("latte_pastorizzato_effettivo", latte_pastorizzato=latte_pastorizzato),
+    "stampa_report("latte_non_pastorizzato", latte_avanzato=latte_avanzato),
     "ricavo_latte_crudo": "Abbiamo ricavato {vendita_latte_crudo:.2f} Euro dalla vendità del latte crudo",  # CAMBIO
     "ricavo_latte_pastorizzato": "Abbiamo ricavato {vendita_latte_pastorizzato:.2f} Euro dalla vendità del latte pastorizzato",  # CAMBIO
     "report_yogurt": "Yogurt prodotto:{TotYogurtProdotto:.2f} Kg \nGuadagno yogurt prodottto {TotGudaganoVenditaInKG:.2f} Euro.\nE stato venduto a {costoKg} euro al KG.\nAbbiamo avuto una riuscita di {scarto_in_percentuale} %",  # CAMBIO
